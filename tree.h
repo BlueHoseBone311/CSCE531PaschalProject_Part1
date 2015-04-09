24b675e6bca53a778c3397fff56f4fbcf5ffa16f
Chris Greer <cagreer@email.sc.edu>
2015-04-09 18:57:55 -0400

    New Tree Functions
    Added structs and functions for expression creation

--- tree.h
+++ tree.h
@@ -12,13 +12,82 @@
 #include "message.h"
 
 
+//Project I DEFINITIONS
 //definitions for a list of multiple variables
 typedef struct var_id {
     ST_ID id;
     struct var_id *next;
 } VAR_ID, *VAR_ID_LIST;
 
+//Project II DEFINITIONS
 
+typedef enum {
+    INTCONST, REALCONST, STRCONST, GID, LVAR, LFUN, UNOP, BINOP,
+    FCALL, ERROR, ARRAY_ACCESS
+} EXPR_TAG;
+
+typedef enum {
+    CONVERT_OP, DEREF_OP, ORD_OP, CHR_OP, SUCC_OP, PRED_OP,
+    NOT_OP, UPLUS_OP, UMINUS_OP;
+} EXPR_UNOP;
+
+typedef enum {
+    ADD_OP, SUB_OP, MUL_OP, DIV_OP, MOD_OP, REALDIV_OP, EQ_OP, LESS_OP, LE_OP,
+    GE_OP, GREATER_OP
+} EXPR_BINOP;
+
+typedef enum {
+    NULL_EOF_OP, NULL_EOLN_OP, NIL_OP
+} EXPR_NULLOP;
+
+typedef struct {
+    struct exprnode * expr;
+    ST_ID id;
+} EXPR_ID;
+
+typedef struct exprlistnode {
+    struct exprnode * expr;
+    struct exprlistnode * next;
+} EXPR_LIST_NODE, * EXPR_LIST;
+
+typedef enum { DIR_EXTERNAL, DIR_FORWARD } DIRECTIVE;
+
+typedef struct exprnode {
+    EXPR_TAG tag;
+    TYPE type;
+    union {
+	long intval;
+	double realval;
+	char * strval;
+	ST_ID gid;	    /* For global variables and global functions */
+	struct {            /* For local variables and formal parameters */
+	    BOOLEAN is_ref; /* TRUE if expr is a VAR (reference) parameter */
+	    int offset;     /* storage location relative to frame pointer */
+	    int link_count; /* Number of ref links to follow to find the var */
+	} lvar;
+	struct {            /* For local functions */
+	    char * global_name; /* The assembler entry point label */
+	    int link_count; /* Number of ref links to follow to find the fcn */
+	} lfun;
+	struct {            /* For nullary operators */
+	    EXPR_NULLOP op;
+	} nullop;
+	struct {            /* For unary operators */
+	    EXPR_UNOP op;
+	    struct exprnode * operand;
+	} unop;
+	struct {            /* For binary operators */
+	    EXPR_BINOP op;
+	    struct exprnode * left, * right;
+	} binop;
+	struct {     /* For procedure/function calls, array accesses  */
+	    struct exprnode * function_or_array;
+	    EXPR_LIST args_or_indices;
+	} fcall_or_array_access;
+    } u;
+} EXPR_NODE, *EXPR;
+
+//PROJECT I FUNCTIONS
 void create_typename(ST_ID id,TYPE new_type);
 /*
 	This function creates a new type name record and installs it in on the symbol table.
@@ -80,9 +149,9 @@ INDEX_LIST create_list_from_type(TYPE type);
 */
 TYPE check_unresolved(ST_ID id);
 /*
-	This method builds an unresolved pointer and adds it to the 
+	This method builds an unresolved pointer and adds it to the
 	LL of unresolved pointers
-	It returns the pointer of TYPE. 
+	It returns the pointer of TYPE.
 */
 void resolve_all_ptr();
 /*
@@ -90,10 +159,54 @@ void resolve_all_ptr();
  	An error message is issued for every unresolved pointer.
 */
 
-TYPE check_subrange(long low, long high); 	
+TYPE check_subrange(long low, long high);
 /*
    Method that builds a subrange of type TYSIGNEDLONGINT, making sure
    that low and high are appropriate ranges
 */
+
+//PROJECT II FUNCTIONS
+
+//Functions for making required expressions
+EXPR make_intconst_expr(long val, TYPE type);
+EXPR make_realconst_expr(double val);
+EXPR make_strconst_expr(char * str);
+EXPR make_id_expr(ST_ID id);
+EXPR make_null_expr(EXPR_NULLOP op);
+EXPR make_un_expr(EXPR_UNOP op, EXPR sub);
+EXPR make_bin_expr(EXPR_BINOP op, EXPR left, EXPR right);
+EXPR make_fcall_expr(EXPR func, EXPR_LIST args);
+EXPR make_error_expr();
+
+//Reverses a list of Expr
+EXPR_LIST expr_list_reverse(EXPR_LIST list);
+
+/*Prepend and EXPR onto the front of an Expr list*/
+EXPR_LIST expr_prepend(EXPR expr, EXPR_LIST list);
+
+/*	Checks taht both the lo and hi values are INTCONSTS.
+	Checks to see if the second subrange is higher than the first.
+	IF not returns error.
+*/
+TYPE check_subrange(EXPR lo, EXPR hi);
+
+//Process variable declarations and install ids into symbol table as either GDECL or LDECL
+int process_var_decl(VAR_ID_LIST ids, TYPE type, int cur_offset);
+
+//Builds the function declaration type and installs into the symbol table
+void build_func_decl(ST_ID id, TYPE type, DIRECTIVE dir);
+
+int enter_function(ST_ID id, TYPE type, char *global_func_name);
+
+//Installs parameters that are used in the enter_function
+void install_params(PARAM_LIST list);
+
+
+EXPR check_assign_or_proc_call(EXPR lhs, ST_ID id, EXPR rhs);
+BOOLEAN is_lval(EXPR expr);
+void expr_free(EXPR expr);
+void expr_list_free(EXPR_LIST list);
+char * get_global_func_name(ST_ID id);
+
 #endif
 
